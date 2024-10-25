a5c7d9758ca0a4fc9bd34cd4766b6013a11d0865
GrabinaSergey <dieumortx@gmail.com>
2024-10-25 17:46:27 +0500

    Task1

---
+++ Tyuiu.GrabinaSA.Sprint3.Task1.V7/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.GrabinaSA.Sprint3.Task1.V7.Lib;
+namespace Tyuiu.GrabinaSA.Sprint3.Task1.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 9;
+
+            Console.WriteLine("Переменная a = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
