8554dbdf3b4b1007e54183f9fc5cb64551bbce25
GrabinaSergey <dieumortx@gmail.com>
2024-10-25 18:33:19 +0500

    Task3

---
+++ Tyuiu.GrabinaSA.Sprint3.Task3.V1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.GrabinaSA.Sprint3.Task3.V1.Lib;
+namespace Tyuiu.GrabinaSA.Sprint3.Task3.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("* Исходные данные:                                                       ");
+            Console.WriteLine("*************************************************************************");
+            string value = "have a nice time";
+            char chr = 'a';
+            Console.WriteLine("Исходная строка: " + value);
+            Console.WriteLine("Искомый символ: " + chr);
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("*Результат:                                                             *");
+            Console.WriteLine("*************************************************************************");
+            Console.WriteLine("Найдено символов: " + ds.GetCharCount(value, chr));
+            Console.ReadKey();
+        }
+    }
+}
