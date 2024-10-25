6febf065fc1d7db494ba94fe670abe3fbf32932c
GrabinaSergey <dieumortx@gmail.com>
2024-10-25 17:57:58 +0500

    Task2

---
+++ Tyuiu.GrabinaSA.Sprint3.Task2.V18.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.GrabinaSA.Sprint3.Task2.V18.Lib;
+namespace Tyuiu.GrabinaSA.Sprint3.Task2.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int value = 1;
+            int startValue = 1;
+            int StopValue = 14;
+
+            double res = ds.GetMultiplySeries(value, startValue, StopValue);
+
+            double wait = 0.277;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GrabinaSA.Sprint3.Task2.V18/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.GrabinaSA.Sprint3.Task2.V18.Lib;
+namespace Tyuiu.GrabinaSA.Sprint3.Task2.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 14;
+            Console.WriteLine("Переменная X = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
